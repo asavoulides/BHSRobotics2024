dac9449b78cf777dbc3bcc91925ab3b5618b908c
David <luoda@belmonthill.org>
2024-09-06 14:59:34 -0400

    commit

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -9,6 +9,12 @@ brain  Brain;
 
 //The motor constructor takes motors as (port, ratio, reversed), so for example
 //motor LeftFront = motor(PORT1, ratio6_1, false);
+motor Motor11 = motor(PORT11, ratio6_1, false);
+motor Motor12 = motor(PORT12, ratio6_1, true);
+motor Motor13 = motor(PORT13, ratio18_1, false);
+motor Motor14 = motor(PORT14, ratio6_1, false);
+motor Motor15 = motor(PORT15, ratio6_1, true);
+motor Motor16 = motor(PORT16, ratio18_1, false);
 
 //Add your devices below, and don't forget to do the same in robot-config.h:
 
